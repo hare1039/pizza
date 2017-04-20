de578689990b35eb3c8128c12d252721530c013c
hare1039 <hare39393@gmail.com>
2017-04-20 20:51:17 +0800

    stash-pt

--- js/fileupload.js
+++ js/fileupload.js
@@ -1,22 +1,68 @@
 //inject ngFileUpload and ngImgCrop directives and services.
-var app = angular.module('fileUpload', ['ngFileUpload', 'ngImgCrop']);
+var app2 = angular.module('fileupload_app', ['ngFileUpload', 'ngImgCrop']);
 
-app.controller('MyCtrl', ['$scope', 'Upload', '$timeout', function ($scope, Upload, $timeout) {
+app2.controller('ctrl_fileupload', ['$scope', 'Upload', '$timeout', function ($scope, Upload, $timeout) {
     $scope.upload = function (dataUrl, name) {
-	Upload.upload({
-	    url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
-	    data: {
-		file: Upload.dataUrltoBlob(dataUrl, name)
-	    },
-	}).then(function (response) {
-	    $timeout(function () {
-		$scope.result = response.data;
-	    });
-	}, function (response) {
-	    if (response.status > 0) $scope.errorMsg = response.status
-	        + ': ' + response.data;
-	}, function (evt) {
-	    $scope.progress = parseInt(100.0 * evt.loaded / evt.total);
-	});
-    }
+    	Upload.upload({
+    	    url: 'https://hare1039.ddns.net/webapps/pizza/upload',
+    	    data: {
+    		    file: Upload.dataUrltoBlob(dataUrl, name)
+    	    },
+    	}).then(function (response) {
+    	    $timeout(function () {
+    		    $scope.result = response.data;
+    	    });
+    	}, function (response) {
+    	    if (response.status > 0) $scope.error_msg = response.status
+    	        + ': ' + response.data;
+    	}, function (evt) {
+    	    $scope.progress = parseInt(100.0 * evt.loaded / evt.total);
+    	});
+    };
+    $scope.img1      = './img/pizza.png';
+    $scope.img1width = 30;
+    $scope.img2      = 'http://wonderopolis.org/wp-content/uploads//2015/03/203_f.jpg';
+    $scope.article   = `
+L1
+
+
+
+
+
+
+
+10
+
+
+
+
+
+
+
+
+
+20
+
+
+
+
+
+
+
+
+
+30`;
+
+
+
+
+    $scope.cropper = {};
+    $scope.cropper.sourceImage = null;
+    $scope.cropper.croppedImage   = null;
+    $scope.bounds = {};
+    $scope.bounds.left = 0;
+    $scope.bounds.right = 0;
+    $scope.bounds.top = 0;
+    $scope.bounds.bottom = 0;
+
 }]);
