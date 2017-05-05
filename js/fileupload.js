61347a37d8e631d34d0d5754c5a7d4073420a5df
hare1039 <hare39393@gmail.com>
2017-05-05 10:49:08 +0800

    Saprate main with file upload controller

--- js/fileupload.js
+++ js/fileupload.js
@@ -1,5 +1,5 @@
 //inject ngFileUpload and ngImgCrop directives and services.
-var app2 = angular.module('fileupload_app', ['ngFileUpload', 'ngImgCrop', 'angular-img-cropper']);
+var app2 = angular.module('fileupload_app', ['ngFileUpload', 'ngImgCrop', 'angular-img-cropper', "ngDialog"]);
 
 app2.controller('ctrl_fileupload', ['$scope', 'Upload', '$timeout', function ($scope, Upload, $timeout) {
     $scope.upload = function (dataUrl, name) {
@@ -22,6 +22,11 @@ app2.controller('ctrl_fileupload', ['$scope', 'Upload', '$timeout', function ($s
     	    $scope.progress = parseInt(100.0 * evt.loaded / evt.total);
     	});
     };
+}]);
+
+
+app2.controller("ctrl_main", ["$scope", function($scope){
+    
     $scope.img1      = './img/pizza.png';
     $scope.img1width = 30;
     $scope.img2      = 'http://wonderopolis.org/wp-content/uploads//2015/03/203_f.jpg';
